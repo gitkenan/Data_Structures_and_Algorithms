94f76e3f826135cd21b0442c8374e6cbaa704f74
Kenan Al-Shamie <kenan.mth@gmail.com>
2023-01-30 20:54:15 +0000

    New problem proposal: the gate for the password

---
+++ 6.py
@@ -0,0 +1,13 @@
+# Let's write a function which can solve the following:
+# Given a string of different characters, if there is less than 1 capital letter, refuse to accept the password.
+
+def strictPass(pass):
+    splitPass = pass.split('')
+    for i in splitPass:
+        # if it's not a capital letter, pass
+        # else, return and print pass
+
+    #return and print failure
+
+
+# Hmm... let's take a break. Back tomorrow.
