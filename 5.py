aa2e08f0b1be55df994e437531539ac4475db545
Kenan Al-Shamie <kenan.mth@gmail.com>
2023-01-30 20:47:49 +0000

    Second push of 5 to clarify python3 spec

--- 5.py
+++ 5.py
@@ -1,5 +1,6 @@
 # Given an integer, return the integer with reversed digits.
 # Note: The integer could be either positive or negative.
+# This should be run on Python3 due to the way we are using print.
 
 def theReverser(x):
     stringy = str(x)
