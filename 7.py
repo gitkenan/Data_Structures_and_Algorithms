46f1a94489806eb440624204019820f319b49160
Kenan Al-Shamie <kenan.al-shamie@ibm.com>
2023-10-19 16:15:02 +0100

    Reverse the Linked List

--- 7.py
+++ 7.py
@@ -6,7 +6,7 @@ class Node:
         self.data = data
         self.next = None
 
-# Define the LinkedList class and create a simple linked list.
+# Define the LinkedList class and create a simple linked list, also add a reverse method.
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -20,6 +20,16 @@ class LinkedList:
             while current.next:
                 current = current.next
             current.next = new_node
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
 
     def display(self):
         current = self.head
@@ -35,4 +45,5 @@ linked_list.append(2)
 linked_list.append(3)
 linked_list.append(4)
 linked_list.display()
-
+linked_list.reverse()
+linked_list.display()
