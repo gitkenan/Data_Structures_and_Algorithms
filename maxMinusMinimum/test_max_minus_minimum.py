502c401782e3e67cfeaadfd5d68fbc3cd366ea60
Kenan Al-Shamie <kenan.mth@gmail.com>
2023-10-22 10:36:27 +0100

    Raise type error when string is given

--- maxMinusMinimum/test_max_minus_minimum.py
+++ maxMinusMinimum/test_max_minus_minimum.py
@@ -1,5 +1,5 @@
 import unittest
-from your_code import get_absolute_difference
+from max_minus_minimum import get_absolute_difference
 
 class TestGetAbsoluteDifference(unittest.TestCase):
     def test_positive_numbers(self):
@@ -21,6 +21,15 @@ class TestGetAbsoluteDifference(unittest.TestCase):
     def test_single_element(self):
         result = get_absolute_difference([42])
         self.assertEqual(result, 0)
+
+    def test_repeat_elements(self):
+        result = get_absolute_difference([24, 24, 24, 24])
+        self.assertEqual(result, 0)
+
+    def test_not_array(self):
+        with self.assertRaises(TypeError):
+            get_absolute_difference('Hi there')
+
 
 if __name__ == '__main__':
     unittest.main()
