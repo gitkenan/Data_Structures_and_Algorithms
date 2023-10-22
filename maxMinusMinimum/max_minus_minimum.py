44476a9ebce9b1b80cb37499bcc955e947903d3d
Kenan Al-Shamie <kenan.mth@gmail.com>
2023-10-22 10:23:47 +0100

    Renaming to suit py conventions pt1

--- maxMinusMinimum/max_minus_minimum.py
+++ maxMinusMinimum/max_minus_minimum.py
@@ -1,13 +1,16 @@
 # What about something a bit simpler for a change - finding the absolute difference between the max and mimimum in an array
 
-def get_absolute_difference(arrayOfNumbers):
-    if len(arrayOfNumbers) < 2:
+def get_absolute_difference(array_of_numbers):
+    if len(array_of_numbers) < 2:
         return 0  # If there are fewer than 2 numbers, the absolute difference is 0.
-    
-    return abs(max(arrayOfNumbers) - min(arrayOfNumbers))
+
+    return abs(max(array_of_numbers) - min(array_of_numbers))
 
 if __name__ == "__main__":
     # Example usage:
     numbers = [5, 3, 9, 1, 7]
     result = get_absolute_difference(numbers)
     print("The absolute difference is:", result)
+
+    # Let's try something where it might be negative
+    # trickySet =
