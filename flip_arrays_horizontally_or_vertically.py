70638065890fbda1bad705a7f7f2fb05348c2f04
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-10-29 11:24:09 +0000

    Update flip_arrays_horizontally_or_vertically.py
    Adding the solution

--- flip_arrays_horizontally_or_vertically.py
+++ flip_arrays_horizontally_or_vertically.py
@@ -1,1 +1,35 @@
 # This programme does what the title says it does. 
+
+def flip_array(arr, direction):
+    if direction == 'horizontal':
+        # Flip horizontally (left to right)
+        # row[::-1] is the expression that is evaluated for each 
+        # row. It uses slicing to reverse the order of elements in 
+        # each row. row[::-1] means to start from the end of the 
+        # list (-1 index) and move towards the beginning, 
+        # reversing the order of elements.
+        return [row[::-1] for row in arr]
+    elif direction == 'vertical':
+        # Flip vertically (upside down)
+        return arr[::-1]
+    else:
+        # Handle invalid direction
+        raise ValueError("Invalid direction. Use 'horizontal' or 'vertical'.")
+
+def main():
+    # Example array
+    original_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    # Input prompt
+    direction = input("Enter 'horizontal' or 'vertical' to flip the array: ")
+    
+    try:
+        flipped_array = flip_array(original_array, direction)
+        print("Flipped Array:")
+        for row in flipped_array:
+            print(row)
+    except ValueError as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
