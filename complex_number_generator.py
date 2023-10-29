5478f9c3c5f8e4462333e027ea64d362a6c28330
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-10-29 10:07:17 +0000

    Update complex_number_generator.py
    no need for second fn

--- complex_number_generator.py
+++ complex_number_generator.py
@@ -7,11 +7,3 @@ def random_complex_number_within_abs(value):
     real_part = random.uniform(-value, value)
     imag_part = random.uniform(-value, value)
     return complex(real_part, imag_part)
-
-
-# This function calculates the absolute value of a complex
-# number of the form a + ib where a, b are real numbers
-# and i is the square root of -1. 
-
-def abs_of_x_in_c(a, b):
-    return sqrt(a**2 + b**2)  # ** is the exponential operator
