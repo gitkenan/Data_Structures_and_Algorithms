ebe2a71d4e0f3370de67b743ccecb3204118a644
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-10-29 10:05:41 +0000

    Update complex_number_generator.py
    with abs val calculator

--- complex_number_generator.py
+++ complex_number_generator.py
@@ -1,8 +1,17 @@
-# This programme calculates the absolute value of a complex
+# Let's generate a random complex number within an absolute value 
+
+import random
+from math import sqrt
+
+def random_complex_number_within_abs(value):
+    real_part = random.uniform(-value, value)
+    imag_part = random.uniform(-value, value)
+    return complex(real_part, imag_part)
+
+
+# This function calculates the absolute value of a complex
 # number of the form a + ib where a, b are real numbers
 # and i is the square root of -1. 
-
-from math import sqrt
 
 def abs_of_x_in_c(a, b):
     return sqrt(a**2 + b**2)  # ** is the exponential operator
