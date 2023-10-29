c62815e4633324d0d6e50032b66adea1cfe88e74
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-10-29 10:19:21 +0000

    Update test_complex_number_generator.py
    The output of function is complex

--- test_complex_number_generator.py
+++ test_complex_number_generator.py
@@ -1,8 +1,9 @@
 import unittest
-from complex_number_generator import random_complex_number_within_abs  # Replace 'your_module_name' with the actual name of your module
+# The below imported function is what we're testing in this file
+from complex_number_generator import random_complex_number_within_abs
 
 class TestRandomComplexNumber(unittest.TestCase):
-    def test_random_complex_number_within_abs(self):
+    def test_abs_value_of_output(self):
         # Test that generated complex number is within the specified absolute value is below
         # the _ is a way to repeat a block of code a specific number of times without needing 
         # to refer to the loop index or value. In other words, 'let's loop like normal loops, 
@@ -10,6 +11,10 @@ class TestRandomComplexNumber(unittest.TestCase):
         for _ in range(100):
             random_num = random_complex_number_within_abs(5)
             self.assertLessEqual(abs(random_num), 5)
+            
+    def test_output_is_complex(self):
+        random_num = random_complex_number_within_abs(5)
+        self.assertIsInstance(random_num, complex)
 
 if __name__ == '__main__':
     unittest.main()
