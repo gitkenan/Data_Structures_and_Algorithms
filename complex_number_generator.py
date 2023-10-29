8761de7727e03482d7d65b7fc032a13d1e7baf8f
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-10-29 10:00:11 +0000

    Update complex_number_generator.py

---
+++ complex_number_generator.py
@@ -0,0 +1,8 @@
+# This programme calculates the absolute value of a complex
+# number of the form a + ib where a, b are real numbers
+# and i is the square root of -1. 
+
+from math import sqrt
+
+def abs_of_x_in_c(a, b):
+    return sqrt(a**2 + b**2)  # ** is the exponential operator
