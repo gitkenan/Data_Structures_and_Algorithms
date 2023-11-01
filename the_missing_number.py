cf2f3ba3a15181d18065868b22601cc05b5c1a59
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-11-01 07:46:13 +0000

    Update the_missing_number.py
    Add a way of categorising arrays

--- the_missing_number.py
+++ the_missing_number.py
@@ -2,12 +2,15 @@
 # array of 1 to 100?
 
 def missing_number(arr):
-    for i in range(1, 101):
+    for i in range(1, upper_limit):
         if i in arr:
             pass
+        elif i<upper_limit/20:
+            print("This array has clear issues.")
+            return i
         else:
             return i
 
 array = [j for j in range(1, 100)]
 
-print(missing_number(array))
+print(missing_number(array, 100))
