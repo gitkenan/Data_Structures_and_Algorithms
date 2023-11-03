21af1bc9920ebb222749c9f7615dd1cc8f8c595a
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-11-03 08:26:41 +0000

    Update test_find_the_double.py
    Using the precious self

--- test_find_the_double.py
+++ test_find_the_double.py
@@ -4,28 +4,26 @@
 import unittest
 from find_the_double import duplicate_numbers
 class TestFindTheDouble(unittest.TestCase):
-    def test_duplicate_numbers():
+    def test_duplicate_numbers(self):
         # Test case 1: Array with multiple duplicates
         arr1 = [1, 2, 3, 4, 5, 5, 6, 6, 7, 8, 9, 23, 23, 545, 3463, 235, 2363]
         expected_result1 = [5, 6, 23]
-        assert duplicate_numbers(arr1) == expected_result1, "Test case 1 failed"
+        self.assertIs(duplicate_numbers(arr1), expected_result1)
     
         # Test case 2: Array with no duplicates
         arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         expected_result2 = []
-        assert duplicate_numbers(arr2) == expected_result2, "Test case 2 failed"
+        self.assertIs(duplicate_numbers(arr2), expected_result2)
     
         # Test case 3: Array with a single element (no duplicates)
         arr3 = [42]
         expected_result3 = []
-        assert duplicate_numbers(arr3) == expected_result3, "Test case 3 failed"
+        self.assertIs(duplicate_numbers(arr3), expected_result3)
     
         # Test case 4: Array with all elements being duplicates
         arr4 = [5, 5, 5, 5, 5]
         expected_result4 = [5]
-        assert duplicate_numbers(arr4) == expected_result4, "Test case 4 failed"
-    
-    print("All test cases passed")
+        self.assertIs(duplicate_numbers(arr4), expected_result4)
 
 if __name__ == "__main__":
     unittest.main()
