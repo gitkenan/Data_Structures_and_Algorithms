e09c3dba14bbe50115b1cfcf448076afac9e8cd9
Kenan Al-Shamie <40515782+gitkenan@users.noreply.github.com>
2023-11-07 10:33:05 +0000

    Update complex_number_generator.py
    Add main conditional expression

--- complex_number_generator.py
+++ complex_number_generator.py
@@ -7,3 +7,7 @@ def random_complex_number_within_abs(value):
     real_part = random.uniform(-value, value)
     imag_part = random.uniform(-value, value)
     return complex(real_part, imag_part)
+    
+if __name__ = "__main__":
+    some_value = random.uniform(0, 1000)
+    random_complex_number_within_abs(some_value)
